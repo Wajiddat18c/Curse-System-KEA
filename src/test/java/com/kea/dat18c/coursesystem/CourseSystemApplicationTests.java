cf6fa841c847504a758d7724bb26277b674df1a6
Wajiddat18c <waji0003@stud.kea.dk>
2019-11-04 23:39:40 +0100

    first init

---
+++ src/test/java/com/kea/dat18c/coursesystem/CourseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kea.dat18c.coursesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
