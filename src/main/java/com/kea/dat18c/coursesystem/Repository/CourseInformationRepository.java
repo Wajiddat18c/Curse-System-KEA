6ec68484bde8681b9e095c897512b4116a4234b7
Ali Al sharefi <alix4970@stud.kea.dk>
2019-11-08 09:47:33 +0100

    Crud til CourseInformationRepo

--- src/main/java/com/kea/dat18c/coursesystem/Repository/CourseInformationRepository.java
+++ src/main/java/com/kea/dat18c/coursesystem/Repository/CourseInformationRepository.java
@@ -1,11 +1,9 @@
 package com.kea.dat18c.coursesystem.Repository;
 
-import com.kea.dat18c.coursesystem.Model.Teacher;
+import com.kea.dat18c.coursesystem.Model.CourseInformation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeacherRepository extends CrudRepository<Teacher, String > {
-
-
+public interface CourseInformationRepository extends CrudRepository<CourseInformation, Integer>  {
 }
