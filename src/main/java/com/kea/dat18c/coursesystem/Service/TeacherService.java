70a72aed2cad60a9fa0015c57f91ba67c39f395d
Wajiddat18c <waji0003@stud.kea.dk>
2019-11-08 09:53:00 +0100

    Update TeacherService.java

--- src/main/java/com/kea/dat18c/coursesystem/Service/TeacherService.java
+++ src/main/java/com/kea/dat18c/coursesystem/Service/TeacherService.java
@@ -1,4 +1,35 @@
 package com.kea.dat18c.coursesystem.Service;
 
+import com.kea.dat18c.coursesystem.Model.Teacher;
+import com.kea.dat18c.coursesystem.Repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class TeacherService {
+
+    final TeacherRepository teacherRepository;
+    @Autowired
+    private TeacherService(TeacherRepository teacherRepository){
+        this.teacherRepository = teacherRepository;
+    }
+    public void create(Teacher teacher){
+        teacherRepository.save(teacher);
+    }
+    public void delete(String email){
+        teacherRepository.deleteById(email);
+    }
+    public void update(Teacher teacher){
+        teacherRepository.save(teacher);
+    }
+    public Teacher findById(String email){
+        Optional<Teacher>find = teacherRepository.findById(email);
+        if (find.isPresent()){
+            return find.get();
+        }
+        return null;
+    }
+
 }
