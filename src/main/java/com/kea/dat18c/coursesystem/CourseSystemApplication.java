b560b26c22bcd887a8e7ab9fb3a419d7ae062f35
Ali Al sharefi <alix4970@stud.kea.dk>
2019-11-26 08:53:28 +0100

    Student-APi done

--- src/main/java/com/kea/dat18c/coursesystem/CourseSystemApplication.java
+++ src/main/java/com/kea/dat18c/coursesystem/CourseSystemApplication.java
@@ -1,9 +1,6 @@
 package com.kea.dat18c.coursesystem;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 67625177b97d009bb963cebac5ec6001c3effdea
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,9 +13,5 @@ public class CourseSystemApplication {
         SpringApplication.run(CourseSystemApplication.class, args);
     }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 67625177b97d009bb963cebac5ec6001c3effdea
 
 }
